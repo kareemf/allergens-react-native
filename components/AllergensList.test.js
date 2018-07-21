934a6cca7d85c083c19d1a52885c8287c37af21c
Kareem Francis <francis.kareem@gmail.com>
2018-07-21 15:15:55 -0400

    Wrap spec in describe block

--- components/AllergensList.test.js
+++ components/AllergensList.test.js
@@ -3,21 +3,22 @@ import AllergensList from './AllergensList';
 
 import renderer from 'react-test-renderer';
 
-it('renders without crashing', () => {
-  const rendered = renderer.create( < AllergensList / > ).toJSON();
-  expect(rendered).toBeTruthy();
+describe('AllergensList', () => {
+  it('renders without crashing', () => {
+    const rendered = renderer.create( < AllergensList /> ).toJSON();
+    expect(rendered).toBeTruthy();
+  });
+
+  it('renders correctly', () => {
+    // given jest snapshot data
+    const snapshotData = [{
+      name: 'Foo'
+    }];
+
+    // when component is rendered
+    const rendered = renderer.create( <AllergensList data={snapshotData} /> ).toJSON();
+
+    // then rendered tree matche snapshot
+    expect(rendered).toMatchSnapshot();
+  });
 });
-
-it('renders correctly', () => {
-  // given jest snapshot data
-  const snapshotData = [{
-    name: 'Foo'
-  }];
-
-  // when component is rendered
-  const rendered = renderer.create( <AllergensList data={snapshotData} /> ).toJSON();
-
-  // then rendered tree matches snapshot
-  expect(rendered).toMatchSnapshot();
-});
-
